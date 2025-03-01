d0622a0dc90df127853ebbc5d19bb4eeab3b55b1
JmoulderYell <josh.moulder@yell.com>
2025-03-01 01:25:11 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Welcome to the Creator Platform</h1>
+      <p className="text-lg text-gray-600 mt-4">Discover and support your favorite creators.</p>
+    </div>
+  );
+}
+
+export default App;
